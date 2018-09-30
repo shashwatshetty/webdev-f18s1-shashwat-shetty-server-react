46dbb4cfaaeade6564d2dec5d88e0628d59371de
sha5hw4t <shashwat_shetty@hotmail.com>
2018-09-29 21:44:57 -0400

    course table functionality added

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
-import WhiteBoard from "./components/WhiteBoard";
+import "../node_modules/font-awesome/css/font-awesome.min.css"
+import WhiteBoard from "./containers/WhiteBoard";
 
 
 ReactDOM.render(
