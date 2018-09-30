bdca07e4190d399bef349d63de19256a3544befe
sha5hw4t <shashwat_shetty@hotmail.com>
2018-09-29 23:35:54 -0400

    course card add and delete functionality added

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,17 +1,25 @@
 import React, {Component} from 'react'
 
-export default class CourseCard extends Component{
-    render() {
-        return (
-            <div className="card" styles={{width: '18rem'}}>
-                <img className="card-img-top"
-                     src="https://picsum.photos/300/200"/>
-                <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <p className="card-text">Card text.</p>
-                    <a href="#" className="btn btn-primary">More...</a>
+const CourseCard = ({course, index, deleteCourse}) =>
+    <div id={index} className="card" styles={{width: '18rem'}}>
+        <img className="card-img-top"
+             src="https://picsum.photos/300/200"/>
+        <div className="card-body">
+            <h3 className="card-title">{course.title}</h3>
+            <div className="row">
+                <div className="col-10">
+                    <h7>Modified {(new Date).toLocaleTimeString()}</h7>
+                </div>
+                <div className="col-2">
+                    <button className="btn btn-danger float-right"
+                            onClick={() => deleteCourse(course)}>
+                        <span aria-hidden={"true"}>
+                                    &times;
+                        </span>
+                    </button>
                 </div>
             </div>
-        )
-    }
-}
+        </div>
+    </div>
+
+export default CourseCard
