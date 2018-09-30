a3cde6e12fa8301a4b18031e7c542e502e38c7b9
sha5hw4t <shashwat_shetty@hotmail.com>
2018-09-29 23:39:18 -0400

    made course table a class

--- src/containers/CourseTable.js
+++ src/containers/CourseTable.js
@@ -2,27 +2,34 @@ import React, {Component} from 'react'
 import CourseRow from "../components/CourseRow";
 import CourseAdd from "../components/CourseAdd";
 
-const CourseTable = ({courses, deleteCourse, addCourse}) =>
-    <div>
-        <CourseAdd
-            addCourse={addCourse}/>
-        <table className="table">
-            <thead>
-                <tr>
-                    <th scope="col">Course Title</th>
-                    <th scope="col">Owned By</th>
-                    <th scope="col">Last Modified</th>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                { courses.map((course, key) =>
-                    <CourseRow course={course}
-                               key={key}
-                               deleteCourse={deleteCourse}/>
-                )}
-            </tbody>
-        </table>
-    </div>
+export default class CourseTable extends Component {
+    constructor(props) {
+        super(props);
+    }
 
-export default CourseTable
+    render(){
+        return (
+            <div>
+                <CourseAdd
+                    addCourse={this.props.addCourse}/>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Course Title</th>
+                            <th scope="col">Owned By</th>
+                            <th scope="col">Last Modified</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { this.props.courses.map((course, key) =>
+                            <CourseRow course={course}
+                                       key={key}
+                                       deleteCourse={this.props.deleteCourse}/>
+                        )}
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
