cb168a6fd63947a3a10f6bc5bbc29775c359d91a
sha5hw4t <shashwat_shetty@hotmail.com>
2018-09-30 19:10:19 -0400

    course id obtained from props

--- src/containers/CourseEditor.js
+++ src/containers/CourseEditor.js
@@ -1,6 +1,11 @@
 import React, {Component} from 'react'
 
 export default class CourseEditor extends Component {
+    constructor(props) {
+        super(props);
+        console.log(this.props.courseId)
+    }
+
     render(){
         return (
             <h2>Course Editor</h2>
