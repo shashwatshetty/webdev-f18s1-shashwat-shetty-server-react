0a6ace899cfe39d971eae8db68df6a95a20e6c2c
sha5hw4t <shashwat_shetty@hotmail.com>
2018-10-01 22:59:41 -0400

    corrected font awesome include file

--- src/components/CourseListNav.js
+++ src/components/CourseListNav.js
@@ -14,12 +14,12 @@ const CourseListNav = () =>
                 </div>
 
                 <div className="col-md-6 col-lg-3 form-group">
-                    <div className="float-right">
-                        <Link to="/courses/table/" className="btn btn-primary ">
-                            Table
+                    <div className="row float-right">
+                        <Link to="/courses/table/" className="btn btn-dark">
+                            <i className="fa fa-table"></i>
                         </Link>
-                        <Link to="/courses/grid/" className="btn btn-primary">
-                            Grid
+                        <Link to="/courses/grid/" className="btn btn-dark">
+                            <i className="fa fa-th-large"></i>
                         </Link>
                     </div>
                 </div>
