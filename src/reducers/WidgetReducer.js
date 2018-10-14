e9c57f0bf8a85fe5c3077b753676a374092d27bf
sha5hw4t <shashwat_shetty@hotmail.com>
2018-10-14 15:09:01 -0400

    widget CRUD opertaions for specific action types added

--- src/reducers/WidgetReducer.js
+++ src/reducers/WidgetReducer.js
@@ -1,5 +1,34 @@
-const WidgetReducer = (state = {widgets: []}, action) => {
+import CourseService from "../services/CourseService";
 
+const WidgetReducer = (state = {widgets: []}, action) => {
+    switch(action.type) {
+        case "FIND_ALL_WIDGETS_FOR_TOPIC":
+            return {
+                widgets: CourseService.findAllWidgetsForTopic(action.topic),
+                currentTopic: action.topic
+            };
+
+        case "CREATE_WIDGET":
+            CourseService.createWidget(state.currentTopic, action.widget)
+            return {
+                widgets: CourseService.findAllWidgetsForTopic(action.topic).slice(0),
+                currentTopic: state.currentTopic
+            };
+
+        case "DELETE_WIDGET":
+            CourseService.deleteWidget(state.currentTopic, action.widget)
+            return {
+                widgets: CourseService.findAllWidgetsForTopic(action.topic).slice(0),
+                currentTopic: state.currentTopic
+            };
+
+        case "UPDATE_WIDGET":
+            CourseService.updateWidget(state.currentTopic, action.widget)
+            return {
+                widgets: CourseService.findAllWidgetsForTopic(action.topic).slice(0),
+                currentTopic: state.currentTopic
+            };
+    }
 };
 
 export default WidgetReducer
