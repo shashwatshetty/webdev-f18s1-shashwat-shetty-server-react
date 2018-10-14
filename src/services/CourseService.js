e032ce0e8f914114ef53006b24e71692d26196ca
sha5hw4t <shashwat_shetty@hotmail.com>
2018-10-14 14:35:04 -0400

    changed json file & made it singleton class

--- src/services/CourseService.js
+++ src/services/CourseService.js
@@ -4,19 +4,74 @@ let courses = [
         title: 'CS5200',
         modules: [
             {
-                id: 'W1',
                 title: 'Week 1',
                 lessons: [
                     {
                         title: 'Lesson 1',
                         topics: [
                             {
-                                title: 'Topic 1'
+                                id: '123',
+                                title: 'topic 1',
+                                widgets: [
+                                    {
+                                        id: '456',
+                                        title: 'list 1',
+                                        type: 'LIST',
+                                        options: 'option 1\noption 2\noption 3'
+                                    },
+                                    {
+                                        id: '123',
+                                        title: 'widget 1',
+                                        type: 'HEADING',
+                                        size: 1,
+                                        text: 'Heading 1'
+                                    },
+                                    {
+                                        id: '234',
+                                        title: 'widget 2',
+                                        type: 'HEADING',
+                                        size: 2,
+                                        text: 'Heading 2'
+                                    },
+                                    {
+                                        id: '345',
+                                        title: 'widget 3',
+                                        type: 'HEADING',
+                                        size: 3,
+                                        text: 'Heading 3'
+                                    }
+                                ]
+                            },
+                            {
+                                id: '234',
+                                title: 'topic 2',
+                                widgets: [
+                                    {
+                                        title: 'widget a'
+                                    },
+                                    {
+                                        title: 'widget b'
+                                    },
+                                    {
+                                        title: 'widget c'
+                                    }
+                                ]
                             }
                         ]
                     },
                     {
-                        title: 'Lesson 2'
+                        title: 'Lesson 2',
+                        topics: [
+                            {
+                                title: 'topic a'
+                            },
+                            {
+                                title: 'topic b'
+                            },
+                            {
+                                title: 'topic c'
+                            }
+                        ]
                     },
                     {
                         title: 'Lesson 3'
@@ -24,22 +79,23 @@ let courses = [
                 ]
             },
             {
-                id: 'W2',
                 title: 'Week 2',
                 lessons: [
                     {
-                        title: 'Lesson A'
+                        title: 'Lesson A',
+                        topics: []
                     },
                     {
-                        title: 'Lesson B'
+                        title: 'Lesson B',
+                        topics: []
                     },
                     {
-                        title: 'Lesson C'
+                        title: 'Lesson C',
+                        topics: []
                     }
                 ]
             },
             {
-                id: 'W2',
                 title: 'Week 3',
                 lessons: []
             }
@@ -53,10 +109,10 @@ let courses = [
 ]
 
 export default class CourseService {
-    findAllCourses = () =>
-        courses
+    static findAllCourses = () =>
+        courses;
 
-    findCourseById = id => {
+    static findCourseById = id => {
         let c = {};
         courses.map(course => {
             if (course.id === id) {
@@ -64,25 +120,25 @@ export default class CourseService {
             }
         });
         return c
-    }
+    };
 
-    updateCourse = (id, course) => {
+    static updateCourse = (id, course) => {
         courses = courses.map(c => {
             if (c.id === id) {
                 c = course;
             }
         });
-    }
+    };
 
-    createCourse = course =>
-        courses.push(course)
+    static createCourse = course =>
+        courses.push(course);
 
-    deleteCourse = courseId =>
+    static deleteCourse = courseId =>
         courses = courses.filter(
             course => course.id !== courseId
-        )
+        );
 
-    findAllModules = selectedCourse => {
+    static findAllModules = selectedCourse => {
         let c = {};
         courses.map(course => {
             if (course.id === selectedCourse.id) {
@@ -90,11 +146,11 @@ export default class CourseService {
             }
         });
         return c.modules
-    }
+    };
 
-    findAllLessons = (selectedCourse, selectedModule) => {
+    static findAllLessons = (selectedCourse, selectedModule) => {
         let moduleList = this.findAllModules(selectedCourse);
-        let m = {}
+        let m = {};
         moduleList.map(e => {
             if (e === selectedModule) {
                 m = e;
