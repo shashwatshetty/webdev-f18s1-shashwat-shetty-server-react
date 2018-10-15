82713ad56f9b826f074be4caf69001c4e8f172d0
sha5hw4t <shashwat_shetty@hotmail.com>
2018-10-14 20:57:32 -0400

    widget addition and deletion logic added

--- src/components/WidgetList.js
+++ src/components/WidgetList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import HeadingWidget from "./HeadingWidget";
 
 class WidgetList extends React.Component {
     constructor(props) {
@@ -6,19 +7,60 @@ class WidgetList extends React.Component {
         props.findAllWidgetsForTopic(props.topic);
     }
 
-    render() {
-        return(
-            <ul className="list-group">
-                {
-                    this.props.currentWidgets.map((widget, index) =>
-                        <li key={index}
-                            className="list-group-item">
-                            {widget.title}
-                        </li>
-                    )
-                }
+    componentDidUpdate() {
+        this.props.findAllWidgetsForTopic(this.props.topic)
+    }
 
-            </ul>
+    addWidget = () => {
+        let newWidget = {
+            id: (new Date()).getTime(),
+            title: 'Heading Widget',
+            type: 'HEADING',
+            size: 1,
+            text: 'Default Text'
+        };
+        this.props.createWidget(newWidget);
+    };
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <button id="save"
+                            className="btn btn-success float-right">
+                        <i className="fa fa-save"/>
+                    </button>
+
+                    <button type="button"
+                            className="btn btn-secondary">
+                        Preview
+                    </button>
+                </div>
+                <br/>
+                <ul className="list-group">
+                    {
+                        this.props.currentWidgets.map((widget, index) =>
+                            <li key={index}
+                                className="list-group-item">
+                                <button
+                                    onClick={() => this.props.deleteWidget(widget)}
+                                    className="btn btn-danger float-right">
+                                    Delete
+                                </button>
+                                {widget.type === "HEADING" && <HeadingWidget updateWidget={this.props.updateWidget}
+                                                                             widget={widget}/>}
+                            </li>
+                        )
+                    }
+                </ul>
+                <br/>
+                <div>
+                    <button className="btn btn-danger float-left"
+                            onClick={() => this.addWidget()}>
+                        <i className="fa fa-plus"/>
+                    </button>
+                </div>
+            </div>
         )
     }
 }
