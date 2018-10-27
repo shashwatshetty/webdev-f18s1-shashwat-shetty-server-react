f3f66a083d673b7387c3f3d6530ef1016f0e84f1
sha5hw4t <shashwat_shetty@hotmail.com>
2018-10-26 23:06:42 -0400

    added login, register, profile pages

--- src/containers/WhiteBoard.js
+++ src/containers/WhiteBoard.js
@@ -1,14 +1,23 @@
 import React, {Component} from 'react'
 import {BrowserRouter as Router, Route} from 'react-router-dom'
 import CourseList from "./CourseList";
+import Login from "../components/Login";
+import Profile from "../components/Profile";
+import Register from "../components/Register";
 
 export default class CourseManager extends Component {
     render() {
         return (
             <Router>
                 <div className="container-fluid">
+                    <Route exact path="/register"
+                           render={() => <Register/>}/>
                     <Route exact path="/courses"
                            render={() => <CourseList/>}/>
+                    <Route exact path="/login"
+                           render={() => <Login/>}/>
+                    <Route exact path="/profile"
+                           render={() => <Profile/>}/>
                 </div>
             </Router>
         )
