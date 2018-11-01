7ef1ec8a1d0da3a74dcd543f86e5f0d689118150
sha5hw4t <shashwat_shetty@hotmail.com>
2018-11-01 18:07:55 -0400

    course grid handling CRUD operations using server

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,28 +1,34 @@
 import React from 'react'
 import {Link} from "react-router-dom";
 
-const CourseCard = ({course, index, deleteCourse, updateCourse}) =>
+const CourseCard = ({course, index, deleteCourse, selectCourse}) =>
     <div id={index} className="card" styles={{width: '18rem'}}>
         <img className="card-img-top"
              src="https://picsum.photos/300/200"/>
         <div className="card-body">
-            <h3 className="card-title">{course.title}</h3>
+            <h3 className="card-title justify-content-center">
+                <button className="btn btn-link"
+                        onClick={() => selectCourse(course)}>
+                    {course.title}
+                </button>
+            </h3>
             <div className="row">
-                <div className="col justify-content-center">
+                <div className="col-12">
                     <h7>Modified {(new Date).toLocaleTimeString()}</h7>
                 </div>
-                <div className="form-group">
-                    <button className="btn btn-danger float-right"
-                            onClick={() => deleteCourse(course)}>
-                        Remove
-                    </button>
-                </div>
-                <div className="form-group">
-                    <Link className="btn btn-primary"
-                          onClick={() => updateCourse(course)}
-                          to={`/${course.id}/edit`}>
-                        Edit
-                    </Link>
+                <div className="form-group col-12 justify-content-between">
+                    <div>
+                        <button className="btn btn-danger float-right"
+                                onClick={() => deleteCourse(course)}>
+                            <i className="fa fa-trash"/>
+                        </button>
+                    </div>
+                    <div>
+                        <Link className="btn btn-primary"
+                              to={`/${course.id}/edit`}>
+                            <i className="fa fa-edit"/>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
--- src/containers/CourseGrid.js
+++ src/containers/CourseGrid.js
@@ -1,10 +1,28 @@
 import React, {Component} from 'react'
 import CourseCard from "../components/CourseCard";
 import CourseAdd from "../components/CourseAdd";
+import CourseRow from "../components/CourseRow";
 
-export default class CourseTable extends Component {
+export default class CourseGrid extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            courses: props.courses,
+            selectedCourse: props.selectedCourse,
+            courseTitle: props.title
+        };
+        this.addCourse = this.props.addCourse;
+        this.selectCourse = this.props.selectCourse;
+        this.updateCourse = this.props.updateCourse;
+        this.deleteCourse = this.props.deleteCourse;
+    }
+
+    componentWillReceiveProps(newProps) {
+        this.setState({
+            courses: newProps.courses,
+            selectedCourse: newProps.selectedCourse,
+            courseTitle: newProps.title
+        })
     }
 
     render(){
@@ -13,12 +31,13 @@ export default class CourseTable extends Component {
                 <table className="table">
                     <tbody>
                         <div className="row">
-                            {   this.props.courses.map((course, key) =>
+                            {   this.state.courses.map((course, key) =>
                                 <div className="form-group col-sm-12 col-md-4 col-lg-2">
                                     <CourseCard course={course}
                                                 key={key}
-                                                deleteCourse={this.props.deleteCourse}
-                                                updateCourse={this.props.updateCourse}/>
+                                                updateCourse={this.updateCourse}
+                                                deleteCourse={this.deleteCourse}
+                                                selectCourse={this.selectCourse}/>
                                 </div>
                             )}
                         </div>
